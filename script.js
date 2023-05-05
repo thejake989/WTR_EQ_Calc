1ef0739c7124a1062d087accdf6a7a1eec1e949b
Jake <thejakethree989@gmail.com>
2023-05-05 00:39:29 -0500

    added dehu card

--- script.js
+++ script.js
@@ -46,3 +46,36 @@ function clearData() {
   document.getElementById("bumpOuts").value = "";
   document.getElementById("result").innerHTML = "";
 }
+
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    items: 1,
+    stagePadding: 50,
+    smartSpeed: 450,
+  });
+});
+
+function calculateDehumidifiers() {
+  const lengthInput = document.getElementById("length").value;
+  const widthInput = document.getElementById("width").value;
+  const ceilingHeightInput = document.getElementById("ceilingHeight").value;
+
+  const length = parseDimension(lengthInput);
+  const width = parseDimension(widthInput);
+  const ceilingHeight = parseDimension(ceilingHeightInput);
+
+  if (length === null || width === null || ceilingHeight === null) {
+    alert("Please enter valid dimensions (Ft, Inches).");
+    return;
+  }
+
+  const volume = length * width * ceilingHeight;
+  const dehumidifiers = Math.ceil(volume / 7000);
+
+  document.getElementById(
+    "dehumidifierResult"
+  ).innerHTML = `You will need ${dehumidifiers} dehumidifiers for this room.`;
+}
